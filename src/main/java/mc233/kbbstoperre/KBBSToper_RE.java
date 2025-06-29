a76c322f573305f6b3dc7d31be7102591872f8e9
SnowyMC <Snow@mc233.fun>
2025-06-29 19:43:48 +0800

    First

---
+++ src/main/java/mc233/kbbstoperre/KBBSToper_RE.java
@@ -0,0 +1,16 @@
+package mc233.kbbstoperre;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KBBSToper_RE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
